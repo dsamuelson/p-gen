dda23cc989479f782879afcc59e140527222f67b
David Samuelson <dsamuelson89@gmail.com>
2022-02-05 16:24:12 -0700

    Password length established

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment code here
+let generatePassword = function() {
+let passLength = window.prompt("How many characters do you need? (8 min 128 max)");
+passLength = parseInt(passLength);
+if (!passLength) {
+  window.alert("You must choose a number between 8 and 128");
+  return;
+} else if (passLength < 8 || passLength > 128) {
+  window.alert("You must choose a number between 8 and 128");
+  return;
+}
+let capLetter = window.confirm("Do you need CAPITAL letters?");
+let lowLetter = window.confirm("Do you need lowercase letters?");
+let numChar = window.confirm("Do you need nubers?");
+let specChar = window.confirm("Do you need special characters? (?/<>*&!@...)");
 
 
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
