0f8fe6c1625fdeedc5eaac86a30911fd0d5e27bf
David Samuelson <linksn.1fan@gmail.com>
2022-02-05 19:43:55 -0700

    Added confirmation of satisfied conditions

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 // Assignment code here
 
-let randomNumber = function(min, max) {
-  let value = Math.floor(Math.random() * (max - min + 1) + min);
-
-  return value;
-};
-
-let coinFlip = function() {
-  let coinToss = true;
-  if (Math.random() > .5) {
-    coinToss = false;
-  }
-  return coinToss;
-}
-
-
 let generatePassword = function() {
   // Make sure that the password is a number and between 8 and 128 characters
   let passWord = "";
@@ -27,40 +12,96 @@ let generatePassword = function() {
     window.alert("You must choose a number between 8 and 128");
     return;
   }
-  // Set up window prompts and determine if we need to make room for the characters to output
+
+  // Set up window prompts and variables for responses for the confirmation prompt at the end
+  var capYes = "";
+  var lowYes  = "";
+  var numYes = "";
+  var specYes = "";
+
+
   let inputPos = "abcdefghijklmnopqrstuvwxyz"
+  let verifyChar = 0;
   let capLetter = window.confirm("Do you need CAPITAL letters?");
   if (capLetter) {
     inputPos += inputPos.toUpperCase();
-    console.log(inputPos);
+    verifyChar += 1;
+    var capYes = "Capital Letters";
+  } else {
+    var capYes = "No Capital Letters";
   }
   let lowLetter = window.confirm("Do you need lowercase letters?");
   if (!lowLetter) {
     inputPos = inputPos.replace(/abcdefghijklmnopqrstuvwxyz/g,'');
-    console.log(inputPos);
+    var lowYes = "No Lowercase Letters"
+  } else {
+    verifyChar += 1;
+    var lowYes = "Lowercase Letters";
   }
   let numChar = window.confirm("Do you need numbers?");
   if (numChar) {
     inputPos += "1234567890";
-    console.log(inputPos);
+    verifyChar += 1;
+    var numYes = "Numbers"
+  } else {
+    var numYes = "No Numbers"
   }
   let specChar = window.confirm("Do you need special characters? (?/<>*&!@...)");
   if (specChar) {
     inputPos += "!@#$%^&*()<>?";
-    console.log(inputPos);
-  }
-  // Set up the password by inserting characters into random slots in the array
-  if (inputPos.length > 0) {
-    for (let i = 0, n = inputPos.length; i < passLength; i++) {
-      passWord += inputPos.charAt(Math.floor(Math.random() * n));
-    }
-    return passWord;
+    verifyChar += 1;
+    var specYes = "Special Characters";
   } else {
-    passWord = "Unable to form Password"
-    return passWord;
+    var specYes = "No Special Characters";
   }
-  
+  // Confirmation prompt
 
+  window.alert("You want a password that has " + passLength + " Characters, which include " + capYes + ", " + lowYes + ", " + numYes + ", and " + specYes + ".")
+
+  getPassWord();
+
+  // Set up the randomly selecting characters from the input string and put in function so it can be called if all parameters aren't satisfied
+
+  function getPassWord () {
+    if (inputPos.length > 0) {
+      for (let i = 0, n = inputPos.length; i < passLength; i++) {
+        passWord += inputPos.charAt(Math.floor(Math.random() * n));
+      }
+    } else {
+      passWord = "Unable to form Password";
+    }
+
+    // Verify that every Character that was selected is at least present once in the password
+
+    let verifyFound = 0;
+    if (capLetter) {
+      /A-Z/.test(passWord);
+      verifyFound += 1;
+    }
+    if (lowLetter) {
+      /a-z/.test(passWord);
+      verifyFound += 1;
+    }
+    if (numChar) {
+      /\d+/.test(passWord);
+      verifyFound += 1;
+    }
+    if (specChar) {
+      /\!\@\#\$\%\^\&\*\(\)\<\>\?/.test(passWord);
+      verifyFound += 1;
+    }
+
+     // If all characters selected are found then the password will print, otherwise it will run again
+
+    if (verifyFound === verifyChar) {
+      console.log(verifyChar + verifyFound);
+      return passWord;
+    } else {
+      getPassWord();
+    }
+  }
+
+  return passWord;
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
